22d342b86b06fb40173673a5bd7fb378b07a97ce
Anchihsin <35755824+Anchihsin@users.noreply.github.com>
2018-02-19 03:30:12 -0800

    Update Patch.js

--- Patch.js
+++ Patch.js
@@ -1,39 +1,24 @@
-function zamfiPatch(x, y) {
+function ANCHIPatch(x, y) {
   noFill();
-  stroke(238);
-  rect(x, y, 100, 100);
+  stroke(0, 150, 255);
+  rect(x, y, 200, 200);
   
-  // blocky J
-  fill(238);
-  noStroke();
-  rect(x+20, y+20, 60, 20);
-  rect(x+40, y+40, 20, 40);
-  rect(x+20, y+60, 40, 20);
-  
-  // overlay of lines
+// overlay of lines
   stroke(0);
   var lines = 3;
-  while (lines < 50) {
-    line(x+lines, y, x, y+lines);
+  while (lines < 200) {
+    line(x+lines, y+50, x+50, y+lines);
     lines = lines + 5;
   }
-  stroke(200);
-  while (lines < 100) {
-    line(x+lines, y, x, y+lines);
-    lines = lines + 3;
-  }
-  stroke(255, 127, 0);
+ 
+  stroke(120, 50, 255);
   lines = 0;
-  while (lines < 50) {
+  while (lines < 200) {
     line(x+100, y+lines, x+lines, y+100);
     lines = lines + 4;
   }
-  stroke(0, 64, 127);
-  while (lines < 100) {
-    line(x+100, y+lines, x+lines, y+100);
-    lines = lines + 4;
-  }
+  
 }
 
-zamfiPatch(20, 30) 
-  
+background(255);
+ANCHIPatch(80, 80); 
